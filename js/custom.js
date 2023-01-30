ade26cd22b6dc7a1dbdd969d978e57c9cc1d5aca
Umesh Chudasama <umeshchudasama1121@gmail.com>
2023-01-30 15:20:14 +0530

    Assignment 3

---
+++ js/custom.js
@@ -0,0 +1,32 @@
+
+  var fileInput = document.getElementById('upload');
+  var noFileSelectedText = 'No file selected';
+  fileInput.addEventListener('change', function(e) {
+      var fileName = '';
+      if (this.files && this.files.length > 1) {
+          fileName = (this.getAttribute('data-multiple-caption') || '').replace('{count}', this.files.length);
+      } else {
+          fileName = e.target.value.split('\\').pop();
+      }
+      if (fileName) {
+          document.getElementById('filename').innerHTML = fileName;
+      } else {
+          document.getElementById('filename').innerHTML = noFileSelectedText;
+      }
+  });
+
+
+// var fileInput = document.getElementById('upload');
+// var noFileSelectedText = 'no file selected';
+// fileInput.addEventListener('change', function(e)
+// {
+//     var filename='';
+//     if(this.files && this.files.length >1)
+//     {
+//         filename =(this.getAttribute('data-multiple-caption') || '').replace('{count}',this.files.length);
+
+//     }
+//     else{
+//         filename = e.targe
+//     }
+// })
